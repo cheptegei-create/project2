2864fd7ca37f7694eaf6b52cab8b891a8afb6c33
cheptegei-create <davidchepte@gmail.com>
2020-04-02 16:49:58 +0300

    added attributes

--- js/main.js
+++ js/main.js
@@ -1,22 +1,39 @@
-var input = document.getElementById("myInput");
-input.addEventListener("keyup", function(event) {
-    if (event.keyCode === 13) {
-        event.preventDefault();
-        document.getElementById("myBtn").click();
-    }
+$(document).ready(function() {
+    $("#myBirthDate").mask("99/99/9999");
 });
 
-function myFunction() {
-    var d = new Date();
-    var weekday = new Array(7);
-    weekday[0] = "Sunday";
-    weekday[1] = "Monday";
-    weekday[2] = "Tuesday";
-    weekday[3] = "Wednesday";
-    weekday[4] = "Thursday";
-    weekday[5] = "Friday";
-    weekday[6] = "Saturday";
+function getAkanName() {
+    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+    var myBirthday = document.getElementById("myBirthDate").value;
+    var myGender = document.getElementsByName("gender");
+    var dateOfBirth = new Date(myBirthday);
+    var dayOfTheWeek = dateOfBirth.getDay();
+    /*Upon submition the application to execute the following command*/
+    /*The application should use the above data to calculate the Akan Name of the user*/
+    if (myBirthday === "") {
+        document.getElementById('message').innerHTML = "Invalid date of birth entry";
+    }
+    /*When the birthday form is filled and the gender selected, the application should execute the following command*/
+    else {
+        for (var i = 0; i < myGender.length; i++) {
+            if (myGender[i].checked) {
+                if (myGender[i].value === "Male") {
+                    document.getElementById('message').innerHTML = "<span> Born on a <span>" + days[dayOfTheWeek] + "</span>, Your Akan Name's <span>" + maleAkanNames[dayOfTheWeek] + "</span>";
+                } else {
+                    document.getElementById('message').innerHTML = "<span> Born on a <span>" + days[dayOfTheWeek] + "</span>, Your Akan Name's <span>" + femaleAkanNames[dayOfTheWeek] + "</span>";
+                }
+                break;
+            }
+            /*If gender is not selected the application to execute the following command*/
+            else {
+                document.getElementById('message').innerHTML = "You Should Select a Gender Too Determine Your Akan Name!";
+            }
+        }
+    }
+}
 
-    var n = weekday[d.getDay()];
-    document.getElementById("demo").innerHTML = n;
+function clearAkanMessage() {
+    document.getElementById('message').innerHTML = "";
 }
